c56953a0c0ce016245f6ab34afedf130828d86bd
kamaleshsevaraj <118503734+kamaleshsevaraj@users.noreply.github.com>
2024-07-03 11:54:56 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// Example JavaScript functionality
+
+// Function to handle a click event
+function handleClick() {
+    alert('You clicked the profile picture!');
+}
+
+
+// Add event listener to the profile picture
+document.querySelector('.profile-pic img').addEventListener('click', handleClick);
